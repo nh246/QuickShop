8a044f86ac6d8392010e12541011df9490d10f8b
nh246 <nazmul210130@diit.edu.bd>
2025-10-27 15:49:55 +0600

    addproduct function added

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -1,6 +1,9 @@
+import connectDB from "@/config/db";
 import authSeller from "@/lib/authSeller";
+import Product from "@/models/Product";
 import { getAuth } from "@clerk/nextjs/server";
 import { v2 as cloudinary } from "cloudinary";
+import { NextResponse } from "next/server";
 
 // configure cloudinary
 
@@ -23,14 +26,14 @@ export async function POST(request) {
       });
     }
 
-    const fromData = await request.fromData();
-    const name = fromData.get("name");
-    const description = fromData.get("description");
-    const category = fromData.get("category");
-    const price = fromData.get("price");
-    const offerPrice = fromData.get("offerPrice");
+    const formData = await request.formData();
+    const name = formData.get("name");
+    const description = formData.get("description");
+    const category = formData.get("category");
+    const price = formData.get("price");
+    const offerPrice = formData.get("offerPrice");
 
-    const files = fromData.getAll("images");
+    const files = formData.getAll("images");
 
     if (!files || files.length === 0) {
       return NextResponse.json({
@@ -44,7 +47,7 @@ export async function POST(request) {
         const arrayBuffer = await file.arrayBuffer();
         const buffer = Buffer.from(arrayBuffer);
 
-        return Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
           const stream = cloudinary.uploader.upload_stream(
             { resource_type: "auto" },
             (error, result) => {
@@ -61,5 +64,28 @@ export async function POST(request) {
     );
 
     const image = result.map((result) => result.secure_url);
-  } catch (error) {}
+
+    await connectDB();
+    const newProduct = await Product.create({
+      userId,
+      name,
+      description,
+      category,
+      price: Number(price),
+      offerPrice: Number(offerPrice),
+      image,
+      date: Date.now(),
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Product added",
+      newProduct,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
